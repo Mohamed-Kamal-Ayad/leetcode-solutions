2fb88fdad3c265779f5efc5fafab2b797b618d08
Mohamed Kamal <99005822+Mohamed-Kamal-Ayad@users.noreply.github.com>
2024-08-11 13:09:37 +0300

    Time: 0 ms (100.00%) | Memory: 9 MB (47.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        /*
-            - make empty string
-            - loop over string
-                - if not alnum char continue
-                - else if isUpper then convert to lower and push
-                - else push direct
-            - end loop
+        int i = 0;
+        int j = s.size() - 1;
 
-            -init 2 pointers
-                - p1 -> start string
-                - p2 -> end string
-            - loop over string
-                - compare each pointer
-                -   if not equal return false
-                -   else equal then p1++ , p2--
-            -end loop
-            return true;
-         */
-
-        string res;
-        for (char c : s) {
-            if (!isalnum(c))
-                continue;
-            else if (isupper(c))
-                res += tolower(c);
-            else
-                res += c;
-        }
-
-        int p1 = 0;
-        int p2 = res.length() - 1;
-
-        for (int i = 0; i < res.length(); i++) {
-            if (res[p1] != res[p2])
+        while (i < j) {
+            while (!isalnum(s[i]) && i < j) {
+                i++;
+            }
+            while (!isalnum(s[j]) && i < j) {
+                j--;
+            }
+            if (tolower(s[i]) != tolower(s[j])) {
                 return false;
-            else
-                p1++, p2--;
+            }
+            i++;
+            j--;
         }
 
         return true;
