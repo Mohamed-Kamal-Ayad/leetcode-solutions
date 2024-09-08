8fa88d38e0f2484ff10995e7f436cd1084d6b023
Mohamed Kamal <99005822+Mohamed-Kamal-Ayad@users.noreply.github.com>
2024-09-08 18:24:21 +0300

    Time: 33 ms (20.28%) | Memory: 22.8 MB (49.35%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -11,26 +11,49 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        deque<ListNode*> lists;
-        ListNode* curr = head->next;
-        while (curr != NULL) {
-            lists.push_back(curr);
-            curr = curr->next;
+
+        if (head->next == NULL) {
+            return;
         }
-        ListNode* f;
-        ListNode* b;
-        ListNode* nlist = head;
-        while (!lists.empty()) {
-            f = lists.front();
-            b = lists.back();
-            b->next = f;
-            nlist->next = b;
-            nlist = f;
-            nlist->next = NULL;
-            lists.pop_back();
-            if (!lists.empty()) {
-                lists.pop_front();
-            }
+
+        ListNode* prev = NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        prev->next = NULL;
+
+        merge(head, reverse(slow));
+    }
+
+private:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    void merge(ListNode* l1, ListNode* l2) {
+        while (l1) {
+            ListNode* tmp1 = l1->next;
+            ListNode* tmp2 = l2->next;
+            l1->next = l2;
+            if (!tmp1) break;
+            l2->next = tmp1;
+            l1 = tmp1;
+            l2 = tmp2;
         }
     }
 };
