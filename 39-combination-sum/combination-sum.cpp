f4e98ab6acaa0ddf48fa9ad201b73d18d2ebe566
Mohamed Kamal <99005822+Mohamed-Kamal-Ayad@users.noreply.github.com>
2025-08-09 08:52:19 +0300

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.81%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(vector<int>& candidates, int target, int start, int& sum,
+             vector<int>& subset, vector<vector<int>>& res) {
+        if (sum > target) {
+            return;
+        }
+
+        if (sum == target) {
+            if (find(res.begin(), res.end(), subset) == res.end())
+                res.push_back(subset);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); ++i) {
+            sum += candidates[i];
+            subset.push_back(candidates[i]);
+            dfs(candidates, target, i, sum, subset, res);
+            sum -= candidates[i];
+            subset.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int sum = 0;
+        vector<int> subset;
+        vector<vector<int>> res;
+        dfs(candidates, target, 0, sum, subset, res);
+        return res;
+    }
+};
