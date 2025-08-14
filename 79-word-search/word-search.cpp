b51c7733fcecb1a362019bed66299ff1dda53cde
Mohamed Kamal <99005822+Mohamed-Kamal-Ayad@users.noreply.github.com>
2025-08-14 18:14:21 +0300

    Time: 950 ms (9.88%) | Memory: 11.8 MB (23.67%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
-    void backtrack(vector<vector<char>>& board, string word, int i,
-                   vector<vector<bool>>& visited, int row, int col,
-                   bool& found) {
+    bool backtrack(vector<vector<char>>& board, string word, int i,
+                   vector<vector<bool>>& visited, int row, int col) {
 
         if (i == word.length()) {
-            found = true;
-            return;
+            return true;
         }
 
-        if (found || row < 0 || row >= board.size() || col < 0 ||
+        if (row < 0 || row >= board.size() || col < 0 ||
             col >= board.front().size() || board[row][col] != word[i] ||
             visited[row][col]) {
-            return;
+            return false;
         }
 
         int dx[] = {0, 1, 0, -1};
@@ -21,24 +19,25 @@ public:
             int newRow = row + dx[d];
             int newCol = col + dy[d];
             visited[row][col] = true;
-            backtrack(board, word, i + 1, visited, newRow, newCol, found);
+            if (backtrack(board, word, i + 1, visited, newRow, newCol)) {
+                return true;
+            }
             visited[row][col] = false;
         }
+        return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         vector<vector<bool>> visited(board.size(),
                                      vector<bool>(board.front().size(), false));
-        bool found = false;
         for (int i = 0; i < board.size(); i++) {
             for (int j = 0; j < board.front().size(); j++) {
                 if (board[i][j] == word[0]) {
-                    backtrack(board, word, 0, visited, i, j, found);
-                    if (found)
-                        break;
+                    if (backtrack(board, word, 0, visited, i, j))
+                        return true;
                 }
             }
         }
-        return found;
+        return false;
     }
 };
