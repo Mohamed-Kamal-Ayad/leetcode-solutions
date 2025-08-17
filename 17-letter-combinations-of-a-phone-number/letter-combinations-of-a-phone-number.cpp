7b5a08e8b26f3e1a584810276418859e02d28daa
Mohamed Kamal <99005822+Mohamed-Kamal-Ayad@users.noreply.github.com>
2025-08-17 13:32:08 +0300

    Time: 1 ms (20.66%) | Memory: 9.4 MB (39.61%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
-
 public:
     vector<string> letterCombinations(string digits) {
         vector<string> res;
         string s;
-        backtrack(digits, 0, res, s);
+        if (!digits.empty())
+            backtrack(digits, 0, res, s);
         return res;
     }
 
@@ -13,21 +13,14 @@ private:
                                       {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
                                       {'8', "tuv"}, {'9', "wxyz"}};
 
-    void backtrack(string digits, int start, vector<string>& res, string& s) {
-        if (start == digits.length()) {
-            if (s.length() == digits.length() && !s.empty()) {
-                res.push_back(s);
-            }
+    void backtrack(string digits, int i, vector<string>& res, string s) {
+        if (s.size() == digits.length()) {
+            res.push_back(s);
             return;
         }
 
-        for (int i = start; i < digits.size(); i++) {
-            string ds = phone[digits[i]];
-            for (char c : ds) {
-                s.push_back(c);
-                backtrack(digits, i + 1, res, s);
-                s.pop_back();
-            }
+        for (char c : phone[digits[i]]) {
+            backtrack(digits, i + 1, res, s + c);
         }
     }
 };
