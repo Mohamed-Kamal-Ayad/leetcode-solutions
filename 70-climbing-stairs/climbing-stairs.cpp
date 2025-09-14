fbec9a346b0d0af1739fef3d576bc4d593cd5fed
Mohamed Kamal <99005822+Mohamed-Kamal-Ayad@users.noreply.github.com>
2025-09-14 16:33:45 +0300

    Time: 0 ms (100.00%) | Memory: 8 MB (59.28%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    vector<int> dpVec = vector<int>(46, -1);
+
+public:
+    int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        if (dpVec[n] != -1)
+            return dpVec[n];
+        dpVec[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return dpVec[n];
+    }
+};
